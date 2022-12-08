e94a83ecf17d8f8accf53a764d0d44cb1f1a5f12
omguptalko <119674842+omguptalko@users.noreply.github.com>
2022-12-08 18:32:22 +0530

    Add files via upload

---
+++ aa.c
@@ -0,0 +1,15 @@
+// Program check the number even and odd
+#include<stdio.h>
+void main()
+{
+int a,b,c,d  ;
+float ratio;
+printf(" enter the four entiger value\n");
+scanf("%d%d%d%d", &a,&b,&c,&d);
+if(  c-d !=0)
+{
+ratio= (float) (a+b) /(float)( c-d);
+printf("Ratio +%f\n", ratio);
+}
+}
+ 
